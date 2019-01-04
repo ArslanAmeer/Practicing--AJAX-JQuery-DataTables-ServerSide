e4ea5f55411eccc6f3d16bc2f966ed6ab5e6ec4b
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-04 15:32:44 +0500

    ✨➕ Add: Model Class & DB context Link

---
+++ Practicing-AJAX-JQuery/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practicing_AJAX_JQuery.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Registration Number")]
+        public string RegistrationNumb { get; set; }
+
+    }
+}
---
+++ Practicing-AJAX-JQuery/Models/StudentContextClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Practicing_AJAX_JQuery.Models
+{
+    public class StudentContextClass : DbContext
+    {
+        public StudentContextClass() : base("ConStr")
+        { }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
