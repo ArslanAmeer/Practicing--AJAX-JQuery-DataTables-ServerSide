499e4e749ff1025cd33c40c3a64b8cb3b0680563
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-04 15:41:34 +0500

    ✨👌✔ Add: Fetching Pre-Exsisting database to Home/Index page

---
+++ Practicing-AJAX-JQuery/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practicing_AJAX_JQuery.Models;
+
+namespace Practicing_AJAX_JQuery.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            StudentContextClass db = new StudentContextClass();
+            List<Student> students;
+            using (db)
+            {
+                students = (from s in db.Students select s).ToList();
+            }
+            return View(students);
+        }
+    }
+}
