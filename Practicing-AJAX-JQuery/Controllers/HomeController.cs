8642248e1fff4fca9d90b695eae1033ef77068c0
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-04 17:30:48 +0500

    ✨👌✔ Add: Getting Data through AJAX call - json.

--- Practicing-AJAX-JQuery/Controllers/HomeController.cs
+++ Practicing-AJAX-JQuery/Controllers/HomeController.cs
@@ -12,13 +12,31 @@ namespace Practicing_AJAX_JQuery.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            StudentContextClass db = new StudentContextClass();
-            List<Student> students;
-            using (db)
-            {
-                students = (from s in db.Students select s).ToList();
-            }
-            return View(students);
+
+            return View();
         }
+
+        public JsonResult GetData()
+        {
+            StudentContextClass db = new StudentContextClass();
+            try
+            {
+                using (db)
+                {
+                    List<Student> students = (from s in db.Students select s).ToList();
+                    if (students.Count > 0)
+                    {
+                        return Json(students, JsonRequestBehavior.AllowGet);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new { msg = "Cannot Fetch Data From Database. SERVER Error" }, JsonRequestBehavior.AllowGet);
+            }
+
+            return null;
+        }
+
     }
 }
