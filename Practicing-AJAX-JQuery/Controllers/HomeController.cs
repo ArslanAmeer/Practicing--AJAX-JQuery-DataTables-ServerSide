0acbc69b2473fff67e63d2d34cfd03e4eee6c2b2
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-22 18:15:10 +0500

    ✨👌 Add: adding Temp table data to Databse

--- Practicing-AJAX-JQuery/Controllers/HomeController.cs
+++ Practicing-AJAX-JQuery/Controllers/HomeController.cs
@@ -38,5 +38,40 @@ namespace Practicing_AJAX_JQuery.Controllers
             return null;
         }
 
+        [HttpPost]
+        public JsonResult AddStudents(string[][] studentsArry)
+        {
+            StudentContextClass db = new StudentContextClass();
+
+            if (studentsArry != null && studentsArry.Length > 0)
+            {
+                List<Student> students = new List<Student>();
+                for (int i = 0; i < studentsArry.Length; i++)
+                {
+                    Student stu = new Student
+                    {
+                        Name = studentsArry[i][1],
+                        RegistrationNumb = studentsArry[i][2]
+                    };
+                    students.Add(stu);
+                    students.TrimExcess();
+                }
+
+                List<Student> oldStudentsList;
+
+                using (db)
+                {
+                    oldStudentsList = (from s in db.Students select s).ToList();
+                    students.RemoveAll(x => oldStudentsList.Exists(y => y.Name == x.Name || y.RegistrationNumb == x.RegistrationNumb));
+                    db.Students.AddRange(students);
+                    db.SaveChanges();
+                }
+
+                return Json(new { success = true, responseText = "New Data added to Database Successfully!" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { success = false, responseText = "Something Went Wrong!" }, JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
