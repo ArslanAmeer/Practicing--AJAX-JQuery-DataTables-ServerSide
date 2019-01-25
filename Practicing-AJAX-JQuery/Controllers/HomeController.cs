244c734984694885a856026f59cf1a4150c14b20
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-25 17:21:01 +0500

    🎉✨✌ Add: Embed Jquery Datatables with Server. Fully Server Side Processing

--- Practicing-AJAX-JQuery/Controllers/HomeController.cs
+++ Practicing-AJAX-JQuery/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Practicing_AJAX_JQuery.Models;
+using VP_TestPro1.Controllers;
 
 namespace Practicing_AJAX_JQuery.Controllers
 {
@@ -39,6 +40,59 @@ namespace Practicing_AJAX_JQuery.Controllers
             {
                 return Json(new { msg = "Cannot Fetch Data From Database. SERVER Error", err = 1 }, JsonRequestBehavior.AllowGet);
             }
+
+        }
+
+        // Fully Server Sourced DataTable Workout
+
+        public JsonResult GetDataToDataTables(DataTablesParam param)
+        {
+
+            int pageNo = 1;
+            int totalCount = 0;
+
+            if (param.iDisplayStart >= param.iDisplayLength)
+            {
+                pageNo = (param.iDisplayStart / param.iDisplayLength) + 1;
+            }
+
+            StudentContextClass db = new StudentContextClass();
+            List<Student> list;
+            using (db)
+            {
+                if (param.sSearch != null)
+                {
+                    totalCount = (from s in db.Students.Where(x =>
+                            x.Name.Contains(param.sSearch) || x.RegistrationNumb.Contains(param.sSearch))
+                                  select s).Count();
+
+                    list = (from s in db.Students
+                            .Where(x => x.Name.Contains(param.sSearch) || x.RegistrationNumb.Contains(param.sSearch))
+                            .OrderBy(x => x.Id)
+                            .Skip((pageNo - 1) * param.iDisplayLength)
+                            .Take(param.iDisplayLength)
+                            select s).ToList();
+                }
+                else
+                {
+                    totalCount = db.Students.Count();
+                    list = (from s in db.Students
+                            .OrderBy(x => x.Id)
+                            .Skip((pageNo - 1) * param.iDisplayLength)
+                            .Take(param.iDisplayLength)
+                            select s).ToList();
+                }
+            }
+
+            return Json(new
+            {
+                aaData = list,
+                sEcho = param.sEcho,
+                iTotalDisplayRecords = totalCount,
+                iTotalRecords = totalCount
+
+            }
+                , JsonRequestBehavior.AllowGet);
 
         }
 
@@ -114,7 +168,7 @@ namespace Practicing_AJAX_JQuery.Controllers
                         db.Entry(oldStudent).State = EntityState.Modified;
                         db.SaveChanges();
                     }
-                    return Json(new { msg = "Successfully Update " + student.Name + " Data", err = 0 }, JsonRequestBehavior.AllowGet);
+                    return Json(new { msg = "Successfully Update <b><u>" + student.Name + "</u></b> Data", err = 0 }, JsonRequestBehavior.AllowGet);
                 }
             }
             catch (Exception e)
---
+++ Practicing-AJAX-JQuery/Models/DataTableParam.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VP_TestPro1.Controllers
+{
+    public class DataTablesParam
+    {
+        public int iDisplayStart { get; set; }
+        public int iDisplayLength { get; set; }
+        public int iColumns { get; set; }
+        public string sSearch { get; set; }
+        public bool bEscapeRegex { get; set; }
+        public int iSortingCols { get; set; }
+        public int sEcho { get; set; }
+        public List<string> sColumnNames { get; set; }
+        public List<bool> bSortable { get; set; }
+        public List<bool> bSearchable { get; set; }
+        public List<string> sSearchValues { get; set; }
+        public List<int> iSortCol { get; set; }
+        public List<string> sSortDir { get; set; }
+        public List<bool> bEscapeRegexColumns { get; set; }
+
+        public DataTablesParam()
+        {
+            sColumnNames = new List<string>();
+            bSortable = new List<bool>();
+            bSearchable = new List<bool>();
+            sSearchValues = new List<string>();
+            iSortCol = new List<int>();
+            sSortDir = new List<string>();
+            bEscapeRegexColumns = new List<bool>();
+        }
+
+        public DataTablesParam(int iColumns)
+        {
+            this.iColumns = iColumns;
+            sColumnNames = new List<string>(iColumns);
+            bSortable = new List<bool>(iColumns);
+            bSearchable = new List<bool>(iColumns);
+            sSearchValues = new List<string>(iColumns);
+            iSortCol = new List<int>(iColumns);
+            sSortDir = new List<string>(iColumns);
+            bEscapeRegexColumns = new List<bool>(iColumns);
+        }
+
+
+    }
+}
